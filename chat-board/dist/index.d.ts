13668f08aa6da3cb8100d8f3ade8132660080be6
--global <kerzo.gm@gmail.com>
2025-12-14 01:15:48 +0200

    Build for last commit

--- chat-board/dist/index.d.ts
+++ chat-board/dist/index.d.ts
@@ -1,4 +1,5 @@
 export { default as MindMap } from './components/mind-map';
+export { default as MindMapChat } from './components/mind-map-chat';
 export { NodeCard } from './components/node-card';
 export { useMindMap } from './hooks/use-mind-map';
 export type { Node, NodeContent, Position } from './lib/types';
